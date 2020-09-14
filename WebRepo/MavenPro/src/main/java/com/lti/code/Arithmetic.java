a528208748a633bc8dab1591dc9ca79b2f711c3e
nakuldevkhajuria <36964792+nakuldevkhajuria@users.noreply.github.com>
2020-09-14 19:49:28 +0530

    Add files via upload

---
+++ WebRepo/MavenPro/src/main/java/com/lti/code/Arithmetic.java
@@ -0,0 +1,16 @@
+package com.lti.code;
+
+public class Arithmetic {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int max(int a, int b) {
+		return a > b ? a : b;
+	}
+	
+	public int min(int a, int b) {
+		return a < b ? a : b;
+	}
+}
---
+++ WebRepo/MavenPro/src/test/java/com/lti/test/TestArithmetic.java
@@ -0,0 +1,37 @@
+package com.lti.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lti.code.Arithmetic;
+
+public class TestArithmetic {
+	private Arithmetic arth;
+	
+	@Before
+	public void init() {
+		arth = new Arithmetic();
+	}
+	
+	@Test
+	public void testSum() {
+		assertEquals(10, arth.sum(4, 6));
+	}
+	
+	@Test
+	public void testSumAgain() {
+		assertEquals(9, arth.sum(4, 5));
+	}
+	
+	@Test
+	public void testMax() {
+		assertEquals(5, arth.max(3, 5));
+	}
+	
+	@Test
+	public void testMin() {
+		assertEquals(3, arth.min(3, 5));
+	}
+}
